8d6241d35560546f9ef4f041ab4176e6cf9b060e
hernandez1981 <kathya_lucia@hotmail.com>
2022-04-14 23:53:27 -0600

    Agregando objeto con propiedades diferentes

---
+++ weekly_mission_2/examples0/examples0-3.js
@@ -0,0 +1,21 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+console.log("Creando un objeto con propiedades diferentes")
+const myObject3 = {
+    name: "Nicolas",
+    firstname: "Hernandez",
+    nicknames: [
+     "Nico",
+      "Coquillo Dante",
+      "Dientes de Oro"
+    ],
+    address: {
+      zip_code: "81200",
+      street: "Praderas del sol",
+      city: "Los Mochis"
+    }
+  }
+  console.log("Ejemplo 3: Objeto con diferentes propiedades")
+  console.log(myObject3)
+  console.log(myObject3.firstname)
+  console.log(myObject3["address"])
+  
