d4924c92dc23bc8cfc65508d0f760525fd6b1e65
hernandez1981 <kathya_lucia@hotmail.com>
2022-04-14 23:37:33 -0600

    Agregando objeto con propiedades

---
+++ weekly_mission_2/examples0/examples0-2.js
@@ -0,0 +1,9 @@
+// Ejemplo 2: Crear un objeto con propiedades
+console.log("Creando objeto con propiedades")
+const myObjetc2 = {
+    name: "Katy",
+    age: 40,
+    city: "Los Mochis"
+  }
+  console.log("Ejemplo 2: Crear un objeto con propiedades")
+  console.log(myObjetc2)
