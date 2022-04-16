a639726503d7e048571c4885a0ade54b40c3de0b
hernandez1981 <kathya_lucia@hotmail.com>
2022-04-15 23:03:48 -0600

    Agregando ejemplo For Each

---
+++ weekly_mission_2/examples1/examples1-1.js
@@ -0,0 +1,6 @@
+// Ejemplo 1: for Each para imprimir elementos de una lista
+console.log("Ejemplo For Each")
+const numbers = [1, 2, 3, 4, 5];
+console.log("Ejemplo 1: Imprimiendo los elementos de una lista")
+numbers.forEach(num => console.log(num))
+
