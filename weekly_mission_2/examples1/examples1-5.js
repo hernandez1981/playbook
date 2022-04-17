90e9943590a1871fbd1723e7deda5b29c4b31390
hernandez1981 <kathya_lucia@hotmail.com>
2022-04-16 22:08:28 -0600

    Ejemplo de Map con toLowerCase

---
+++ weekly_mission_2/examples1/examples1-5.js
@@ -0,0 +1,10 @@
+// Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a minúsculas
+console.log("Uso de map para ver la lista y usando el toLowerCase para convertir la lista a minúsculas")
+const names = ['Katy', 'LuCIA', 'Nicolas', 'JUAN']
+const namesToLowerCase = names.map((name) => name.toLowerCase())
+
+console.log("Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a minúsculas")
+console.log(namesToLowerCase)
+
+
+
