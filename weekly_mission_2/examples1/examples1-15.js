b48492a54f968b11c430059a9754ced7407a9b49
hernandez1981 <kathya_lucia@hotmail.com>
2022-04-17 20:45:03 -0600

    Eejjemplo ordenando lista en orden alfabético con el uso de Sort

---
+++ weekly_mission_2/examples1/examples1-15.js
@@ -0,0 +1,5 @@
+//Ejemplo 15: Uso de sort para ordenar elementos
+console.log("Ordenando la lista de la A-Z mediante Sort")
+const products = ['Milk', 'Coffee', 'Sugar', 'Honey', 'Apple', 'Carrot', 'Lemon', 'Tomatoes']
+console.log("Ejemplo 15: Elementos ordernados usando SORT")
+console.log(products.sort())
