f3db07d3d745fd10e0f0ad0a811fababde8407a9
hernandez1981 <kathya_lucia@hotmail.com>
2022-04-23 20:56:26 -0600

    Modelando información de facebook:objeto Biography

---
+++ weekly_mission_2/exercises/Ejercicio1/facebook/biography.js
@@ -0,0 +1,19 @@
+//Modelando información de facebook objeto biography.
+console.log("Modelando información de facebook: objeto Biography.")
+const biography = {
+    user: "Katy Hernández", 
+    photo: "imagen perfil",
+    timeline: "Estudiante de programación de LaunchX",
+    about: "Acerca de mi",
+    live: "Sinaloa",  
+    updateInfo: new Date(), 
+    ActivityLog: 5,
+    friends: 100, 
+    status: 1,
+    getGeneralInfo: function(){
+        return `Biografía de ${this.user} es ${this.timeline}, vive en ${this.live}, la biografía reciente es de ${this.updateInfo.toLocaleDateString()}`
+    }  
+}
+
+console.log("Nombre del usuario:" + biography.user)
+console.log(biography.getGeneralInfo())
